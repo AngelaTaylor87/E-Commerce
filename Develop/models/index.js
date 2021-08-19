f8ffc1af767608e5b69a8dbff943727240f19ba9
AngelaTaylor87 <82851741+AngelaTaylor87@users.noreply.github.com>
2021-08-19 16:09:15 -0500

    updated indexjs

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,15 +1,27 @@
 // import models
+const { Model, DataTypes } = require('sequelize');
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+class Index extends Model {}
 // Categories have many Products
-
+Product.init
 // Products belongToMany Tags (through ProductTag)
-
+(
+  {
+    // define columns
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'index',
+  }
+);
 // Tags belongToMany Products (through ProductTag)
 
 module.exports = {
